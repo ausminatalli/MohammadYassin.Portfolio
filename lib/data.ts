82e8924e332bc0b10b46608fd60be5d55ee42995
Mohammad Yassine <115090892+ausminatalli@users.noreply.github.com>
2023-10-19 18:26:11 +0300

    aaa

--- lib/data.ts
+++ lib/data.ts
@@ -2,9 +2,9 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
-import ecommerce from "@/public/ecommerce.png";
-import transportation from "../public/transportation.png";
-import hr from "../public/hr.png";
+// import ecommerce from "@/public/;
+// import transportation from "../public/transportation.png";
+// import hr from "../public/hr.png";
 export const links = [
   {
     name: "Home",
@@ -74,7 +74,7 @@ export const projectsData = [
       "SaaS",
       "Cloudinary",
     ],
-    imageUrl: { ecommerce },
+    imageUrl: "/ecommerce.png",
   },
   {
     title: "Transportation Management System",
@@ -90,7 +90,7 @@ export const projectsData = [
       "Strip",
       "Cloudinary",
     ],
-    imageUrl: { transportation },
+    imageUrl: "transportation.png",
   },
   {
     title: "HR Management System",
@@ -105,7 +105,7 @@ export const projectsData = [
       "Auth.js",
       "Sequlize",
     ],
-    imageUrl: { hr },
+    imageUrl: "/hr.png",
   },
 ] as const;
 
