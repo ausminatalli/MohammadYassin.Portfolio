e809dbe34dc597fbc672c00a423a61776bcd8202
Mohammad Yassine <115090892+ausminatalli@users.noreply.github.com>
2023-10-19 19:05:18 +0300

    aaa

--- lib/data.ts
+++ lib/data.ts
@@ -2,9 +2,9 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
-// import Ecommerce from "@/public/ecommerce.png"
-// import Transportation from "@/public/transportation.png";
-// import Hr from "@/public/hr.png";
+import Ecommerce from "@/public/ecommerce.png";
+import Transportation from "@/public/transportation.png";
+import Hr from "@/public/hr.png";
 export const links = [
   {
     name: "Home",
@@ -59,55 +59,55 @@ export const experiencesData = [
   },
 ] as const;
 
-// export const projectsData = [
-//   {
-//     title: "E-commerce",
-//     description:
-//       "The website is an e-commerce platform dedicated to shoes. It was built using React JS and incorporates several essential components, including a product management system, administrative integration, Stripe payment processing, viewer tracking, and a comprehensive logging system.",
-//     tags: [
-//       "Reactjs",
-//       "Nodejs",
-//       "Expressjs",
-//       "Tailwind",
-//       "Redux",
-//       "Strip",
-//       "SaaS",
-//       "Cloudinary",
-//     ],
-//     imageUrl: Ecommerce,
-//   },
-//   {
-//     title: "Transportation Management System",
-//     description:
-//       "I developed an efficient bus transportation system with secure payment capabilities. Admins have the power to manage stations, buses, and travel schedules, while users can securely book tickets. Additionally, drivers can register to work as bus drivers.",
-//     tags: [
-//       "HTML",
-//       "CSS",
-//       "Vanillajs",
-//       "Bootstrap",
-//       "mysql",
-//       "PHP",
-//       "Strip",
-//       "Cloudinary",
-//     ],
-//     imageUrl: Transportation,
-//   },
-//   {
-//     title: "HR Management System",
-//     description:
-//       "Created a web-based HR Management System to facilitate efficient employee-HR interactions, enabling employees to submit requests, HR to respond promptly, and facilitating secure document uploads.",
-//     tags: [
-//       "Next.js",
-//       "Typescrypt",
-//       "Tailwind",
-//       "Cloudinary",
-//       "PDFLib",
-//       "Auth.js",
-//       "Sequlize",
-//     ],
-//     imageUrl: Hr,
-//   },
-// ] as const;
+export const projectsData = [
+  {
+    title: "E-commerce",
+    description:
+      "The website is an e-commerce platform dedicated to shoes. It was built using React JS and incorporates several essential components, including a product management system, administrative integration, Stripe payment processing, viewer tracking, and a comprehensive logging system.",
+    tags: [
+      "Reactjs",
+      "Nodejs",
+      "Expressjs",
+      "Tailwind",
+      "Redux",
+      "Strip",
+      "SaaS",
+      "Cloudinary",
+    ],
+    imageUrl: Ecommerce,
+  },
+  {
+    title: "Transportation Management System",
+    description:
+      "I developed an efficient bus transportation system with secure payment capabilities. Admins have the power to manage stations, buses, and travel schedules, while users can securely book tickets. Additionally, drivers can register to work as bus drivers.",
+    tags: [
+      "HTML",
+      "CSS",
+      "Vanillajs",
+      "Bootstrap",
+      "mysql",
+      "PHP",
+      "Strip",
+      "Cloudinary",
+    ],
+    imageUrl: Transportation,
+  },
+  {
+    title: "HR Management System",
+    description:
+      "Created a web-based HR Management System to facilitate efficient employee-HR interactions, enabling employees to submit requests, HR to respond promptly, and facilitating secure document uploads.",
+    tags: [
+      "Next.js",
+      "Typescrypt",
+      "Tailwind",
+      "Cloudinary",
+      "PDFLib",
+      "Auth.js",
+      "Sequlize",
+    ],
+    imageUrl: Hr,
+  },
+];
 
 export const skillsData = [
   "HTML",
