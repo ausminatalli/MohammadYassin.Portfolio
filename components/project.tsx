0afac06f6a05110365602d7530ad38da5b8d4ad6
Mohammad Yassine <115090892+ausminatalli@users.noreply.github.com>
2023-10-19 18:38:43 +0300

    aa

--- components/project.tsx
+++ components/project.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import { useRef } from "react";
-import { projectsData } from "@/lib/data";
 import Image from "next/image";
 import { motion, useScroll, useTransform } from "framer-motion";
+import { projectsData } from "./data";
 
 type ProjectProps = (typeof projectsData)[number];
 
--- lib/types.ts
+++ lib/types.ts
@@ -1,3 +1,3 @@
-import { links } from "./data";
+import { links } from "../components/data";
 
 export type SectionName = (typeof links)[number]["name"];
